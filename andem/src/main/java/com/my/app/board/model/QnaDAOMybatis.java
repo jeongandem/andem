95458b87ed7a1f52e735b22af9741f83f9bfdeb9
YuTaeKyung <taekung92@naver.com>
2017-01-22 15:27:03 +0900

    민주야 왜그랬어

--- andem/src/main/java/com/my/app/board/model/QnaDAOMybatis.java
+++ andem/src/main/java/com/my/app/board/model/QnaDAOMybatis.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class QnaDAOMybatis implements QnaDAO{
 
+	private String namespace = "";
+	
+	
 	@Override
 	public int insertQna(QnaVO vo) {
 		return 0;
